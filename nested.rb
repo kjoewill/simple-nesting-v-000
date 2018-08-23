a06878bf3f97aac8bffca7987659c3633a767196
Kevin W <kjoewill@gmail.com>
2018-08-23 03:25:34 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -85,8 +85,8 @@ def adding_matz
         }
      }
 
-  programmer_hash[:yukihiro_matsumoto] = {}
-  programmer_hash[:yukihiro_matsumoto][:known_for] = "ruby"
+  programmer_hash[:yukihiro_matsumoto] = {:known_for => "Ruby"}
+  
     
 end
 
